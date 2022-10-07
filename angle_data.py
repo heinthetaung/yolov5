db144cb7c5cf14025fbaa67b8a72e57187f459fa
Hein <heinthetaung.cloud@gmail.com>
2022-10-07 12:09:29 +0700

    update angle_data.py paths

--- angle_data.py
+++ angle_data.py
@@ -6,20 +6,24 @@ import os
 def add_angle_data(img):
     return img
 
-img_sample = ''
-p = Path(img_sample).parent
-img_files = [x for x in p.glob("*.jpg") if x.is_file()]
+img_sample1 = '/home/xyz/Documents/learning/machine-learning/datasets/VisDrone/VisDrone2019-DET-val/images'
+img_sample2 = '/home/xyz/Documents/learning/machine-learning/datasets/VisDrone/VisDrone2019-DET-train/images'
+img_sample3 = '/home/xyz/Documents/learning/machine-learning/datasets/VisDrone/VisDrone2019-DET-test-dev/images'
+img_samples = [img_sample1, img_sample2, img_sample3]
+for img_sample in img_samples:
+    p = Path(img_sample)
+    img_files = [x for x in p.glob("*.jpg") if x.is_file()]
 
-depth_folder = p.parent / 'depth'
-if not os.path.exists(depth_folder):
-    os.makedirs(depth_folder)
-    
-for f in img_files:
-    img = cv2.imread(str(f))
-    depth_img = np.zeros((img.shape[0], img.shape[1]))
-    depth_img = add_angle_data(depth_img)
-    depth_img = np.uint8(depth_img)
-    cv2.imwrite(str((depth_folder / f.stem).with_suffix(".png")), depth_img)
+    depth_folder = p.parent / 'depth'
+    if not os.path.exists(depth_folder):
+        os.makedirs(depth_folder)
+        
+    for f in img_files:
+        img = cv2.imread(str(f))
+        depth_img = np.zeros((img.shape[0], img.shape[1]))
+        depth_img = add_angle_data(depth_img)
+        depth_img = np.uint8(depth_img)
+        cv2.imwrite(str((depth_folder / f.stem).with_suffix(".png")), depth_img)
 
 
 # # reference
