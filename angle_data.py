247137ca68d32ead75061d5402466dd2c7dc1621
Hein <heinthetaung.cloud@gmail.com>
2022-10-08 18:59:27 +0700

    fix bug for all depth images are not generated

--- angle_data.py
+++ angle_data.py
@@ -21,7 +21,7 @@ def add_angle_data(img, angle, height):
 img_sample1 = '../datasets/VisDrone/VisDrone2019-DET-val/images'
 img_sample2 = '../datasets/VisDrone/VisDrone2019-DET-train/images'
 img_sample3 = '../datasets/VisDrone/VisDrone2019-DET-test-dev/images'
-img_samples = [img_sample1, img_sample2, img_sample3]
+img_samples = [img_sample1 img_sample2 img_sample3]
 for img_sample in img_samples:
     p = Path(img_sample)
     img_files = [x for x in p.glob("*.jpg") if x.is_file()]
@@ -30,19 +30,18 @@ for img_sample in img_samples:
     if not os.path.exists(depth_folder):
         os.makedirs(depth_folder)
         
-
-for f in img_files:
-    angle_labels = (p.parent / 'angle_labels' / f.stem).with_suffix('.txt')
-    with open(angle_labels, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        row = next(csv_reader)
-    # print(f)
-    # print(angle_labels)
-    img = cv2.imread(str(f))
-    depth_img = np.zeros((img.shape[0], img.shape[1]))
-    depth_img = add_angle_data(depth_img, row[0], row[1])
-    depth_img = np.uint8(depth_img)
-    cv2.imwrite(str((depth_folder / f.stem).with_suffix(".png")), depth_img)
+    for f in img_files:
+        angle_labels = (p.parent / 'angle_labels' / f.stem).with_suffix('.txt')
+        with open(angle_labels, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            row = next(csv_reader)
+        print(f)
+        # print(angle_labels)
+        img = cv2.imread(str(f))
+        depth_img = np.zeros((img.shape[0], img.shape[1]))
+        depth_img = add_angle_data(depth_img, row[0], row[1])
+        depth_img = np.uint8(depth_img)
+        cv2.imwrite(str((depth_folder / f.stem).with_suffix(".png")), depth_img)
 
 
 # # reference
